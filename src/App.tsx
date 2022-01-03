0d87ec67b1231e9d738bff12c8c8660fddb32190
Zyllio <96659995+zyllio@users.noreply.github.com>
2022-01-03 07:20:21 +0100

    -

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      TEST
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactiveElements from 'reactive-elements';
+
+import './index.css';
+import App from './App';
+
+ReactiveElements('app-component', App, { useShadowDom: true })
